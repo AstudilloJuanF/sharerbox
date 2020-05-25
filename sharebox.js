636e9e52ce826ca619e0b72be65933ce0ed644ee
Juan Astudillo <57147770+AstudilloJuanF@users.noreply.github.com>
2020-05-25 14:43:01 -0400

    Update sharebox.js

--- sharebox.js
+++ sharebox.js
@@ -1,25 +1,26 @@
-// Manipulationg Styles
+// Manipulating Styles
 
-var shareBox =  document.getElementById('share-section');
-var shareIcon = document.getElementById('share-icon');
+	var shareBox =  document.getElementById('share-section');
+	var shareIcon = document.getElementById('share-icon');
 
-function flipIcon(){
-	if(shareIcon.style.transform === 'rotateY(180deg)'){
-		shareIcon.style.transform = 'none';
-		shareBox.style.transform = 'translateX(-45px)';
-	}else{
-		shareIcon.style.transform = 'rotateY(180deg)';
-		shareBox.style.transform = 'none';
+	function flipIcon(){
+		if(shareIcon.style.transform === 'rotateY(180deg)'){
+			shareIcon.style.transform = 'none';
+			shareBox.style.transform = 'translateX(-45px)';
+		}else{
+			shareIcon.style.transform = 'rotateY(180deg)';
+			shareBox.style.transform = 'none';
+		}
 	}
-}
 
-shareIcon.addEventListener('click', flipIcon);
+	shareIcon.addEventListener('click', flipIcon);
 
-var fbLink = document.getElementById('fb-link');
-var twLink = document.getElementById('tw-link');
-var currentUrl = document.URL;
 
 // Setting share links for socialmedia
 
-fbLink.href = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(currentUrl);
-twLink.href = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(currentUrl);
+	var fbLink = document.getElementById('fb-link');
+	var twLink = document.getElementById('tw-link');
+	var currentUrl = document.URL;
+
+	fbLink.href = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(currentUrl);
+	twLink.href = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(currentUrl);
